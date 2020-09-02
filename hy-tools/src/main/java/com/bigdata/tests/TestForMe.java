31e4ef9637dca93989c43d0b69e1543b0ac409ab
huyi <3614322595@qq.com>
2020-09-02 20:16:48 +0800

    更新测试类

--- hy-tools/src/main/java/com/bigdata/tests/TestForMe.java
+++ hy-tools/src/main/java/com/bigdata/tests/TestForMe.java
@@ -1,6 +1,10 @@
 package com.bigdata.tests;
 
-import java.util.concurrent.ThreadLocalRandom;
+import com.bigdata.hy_tools.entity.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Program: hy-utils @ClassName: TestForMe @Author: huyi @Date: 2020-09-02
@@ -8,8 +12,26 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class TestForMe {
   public static void main(String[] args) {
-    while (true) {
-      System.out.println(ThreadLocalRandom.current().nextInt(10) + 1);
-    }
+    //    while (true) {
+    //      System.out.println(ThreadLocalRandom.current().nextInt(10) + 1);
+    //    }
+    List<Data> list =
+        new ArrayList<Data>() {
+          {
+            add(new Data("hy1", 10));
+            add(new Data("hy2", 11));
+          }
+        };
+    list.forEach(System.out::println);
+    list =
+        list.stream()
+            .peek(
+                x -> {
+                  if (x.getName().equals("hy2")) {
+                    x.setMoney(10000);
+                  }
+                })
+            .collect(Collectors.toList());
+    list.forEach(System.out::println);
   }
 }
